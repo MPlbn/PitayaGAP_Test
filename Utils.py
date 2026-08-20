aece85de682edee5b028977fa43da080e309de99
Mikolaj Plebanek <mikolaj.plebanek@gmail.com>
2026-08-20 20:53:17 +0200

    changes to filters

--- Utils.py
+++ Utils.py
@@ -1,11 +1,13 @@
 ## module responsible for providing filtering functions to the data captured in CSV files
+from statistics import median
 
-def filterIN1(uDataSet, uThreshold=20):
+def singleFilter(uDataSet, uThreshold=10, channel = 0):
     i = 1
     counter = 0
     while(i < len(uDataSet) - 1):
-        differenceValue = uDataSet[i][0] - uDataSet[i-1][0]
+        differenceValue = uDataSet[i][channel] - uDataSet[i-1][channel]
         if(abs(differenceValue) > uThreshold):
+            print(f'{uDataSet[i][0]} - {uDataSet[i-1][0]}')
             uDataSet.pop(i)
             counter += 1
         else:
@@ -15,12 +17,12 @@ def filterIN1(uDataSet, uThreshold=20):
 
 
 
-def filterIN2(uDataSet, uThreshold=10):
+def dualFilter(uDataSet, uThreshold=20, channel = 1):
     i = 1
     counter = 0
     while(i < len(uDataSet) - 2):
-        differenceValuePrevious = uDataSet[i][1] - uDataSet[i-1][1]
-        differenceValueNext = uDataSet[i][1] - uDataSet[i+1][1]
+        differenceValuePrevious = uDataSet[i][channel] - uDataSet[i-1][channel]
+        differenceValueNext = uDataSet[i][channel] - uDataSet[i+1][channel]
         if(abs(differenceValuePrevious) > uThreshold and abs(differenceValueNext) > uThreshold):
             uDataSet.pop(i)
             counter += 1
@@ -29,14 +31,34 @@ def filterIN2(uDataSet, uThreshold=10):
 
     print(f'Counter value: {counter}')
 
-def processData(uDataSet, uResistance = 1000):
+
+def medianFilter(uDataSet, channel = 0):
+    filtered = [row[:] for row in uDataSet]
+
+    for i in range(2, len(uDataSet)-2):
+        window = [uDataSet[j][channel] for j in range(i-2, i+3)]
+        filtered[i][channel] = median(window)
+    return filtered
+
+
+def processDataDiode(uDataSet, uResistance = 1000):
     newDataSet = []
 
     for row in uDataSet:
-        VOut = row[0]
-        VDiode = row[1]
-        current = (VOut - VDiode) / uResistance
+        VOut = row[0] #mV
+        VDiode = row[1] #mV
+        current = (VOut - VDiode) / uResistance * 1000 #uA
 
         newDataSet.append([VDiode, current])
 
+    return newDataSet
+
+def processDataResistance(uDataSet, uResistance = 1000):
+    newDataSet = []
+    for row in uDataSet:
+        VOut = row[0] #mV
+        current = VOut / uResistance #mA
+    
+        newDataSet.append([VOut, current])
+    
     return newDataSet
--- test.py
+++ test.py
@@ -2,60 +2,235 @@ import matplotlib.pyplot as plt
 import Utils
 import csv
 
-with open("data/dioda1fast.csv", newline="") as file:
-    reader = csv.reader(file, delimiter=';')
-    next(reader)  # pomija nagłówek
+def g1_s(uTestNumber = 1):
+    path = f'data/S_TEST_{uTestNumber}.csv'
+    with open(path, newline="") as file: #S_TEST_1-3
+            reader = csv.reader(file, delimiter=';')
+            next(reader)  # pomija nagłówek
+    
+            data = [[float(value) for value in row] for row in reader]
 
-    data = [[float(value) for value in row] for row in reader]
+    y = [row[0] for row in data]
 
-## for fast:
-    data = [[value * 1000 for value in row] for row in data]
-##end for fast
+    plt.plot(y)
+    plt.xlabel("t")
+    plt.ylabel("V [mV]")
+    plt.xticks([])
+    plt.grid()
+    plt.show()
 
-x = [row[0] for row in data]
-y = [row[1] for row in data]
+def g1_f():
+    with open("data/F_TEST_1.csv", newline="") as file:
+            reader = csv.reader(file, delimiter=';')
+            next(reader)  # pomija nagłówek
+    
+            data = [[float(value) for value in row] for row in reader]
 
-plt.plot(x, y)
-plt.xlabel("IN1")
-plt.ylabel("IN2")
-plt.grid()
-plt.show()
+            data = [[value * 1000 for value in row] for row in data]
 
-print(f'data length {len(data)}')
+    y = [row[0] for row in data]
 
-Utils.filterIN1(data)
+    plt.plot(y)
+    plt.xlabel("t")
+    plt.ylabel("V [mV]")
+    plt.xticks([])
+    plt.grid()
+    plt.show()
+    
 
-print(f'data length {len(data)}')
+def g2_s():
+    with open("data/S_TEST_4.csv", newline="") as file:
+        reader = csv.reader(file, delimiter=';')
+        next(reader)  # pomija nagłówek
 
-x = [row[0] for row in data]
-y = [row[1] for row in data]
+        data = [[float(value) for value in row] for row in reader]
 
-plt.plot(x, y)
-plt.xlabel("IN1")
-plt.ylabel("IN2")
-plt.grid()
-plt.show()
+    y = [row[0] for row in data]
 
-Utils.filterIN2(data)
+    plt.plot(y)
+    plt.xlabel("t")
+    plt.ylabel("IN1 [mV]")
+    plt.grid()
+    plt.show()
 
-print(f'data length {len(data)}')
+    print(f'data length {len(data)}')
 
-x = [row[0] for row in data]
-y = [row[1] for row in data]
+    data = Utils.medianFilter(data, 0)
 
-plt.plot(x, y)
-plt.xlabel("IN1")
-plt.ylabel("IN2")
-plt.grid()
-plt.show()
+    print(f'data length {len(data)}')
 
-newData = Utils.processData(data)
+    y = [row[0] for row in data]
 
-x = [row[0] for row in newData]
-y = [row[1] for row in newData]
+    plt.plot(y)
+    plt.xlabel("t")
+    plt.ylabel("IN2")
+    plt.grid()
+    plt.show()
 
-plt.plot(x, y)
-plt.xlabel("Voltage [mV]")
-plt.ylabel("Current [uA]")
-plt.grid()
-plt.show()
+    newData = Utils.processDataResistance(data)
+
+    x = [row[0] for row in newData]
+    y = [row[1] for row in newData]
+
+    plt.plot(x, y)
+    plt.xlabel("Voltage [mV]")
+    plt.ylabel("Current [mA]")
+    plt.grid()
+    plt.show()
+
+def g2_f():
+    with open("data/F_TEST_2.csv", newline="") as file:
+        reader = csv.reader(file, delimiter=';')
+        next(reader)  # pomija nagłówek
+
+        data = [[float(value) for value in row] for row in reader]
+
+        data = [[value * 1000 for value in row] for row in data]
+
+    y = [row[0] for row in data]
+
+    plt.plot(y)
+    plt.xlabel("t")
+    plt.ylabel("IN1 [mV]")
+    plt.grid()
+    plt.show()
+
+    print(f'data length {len(data)}')
+
+    data = Utils.medianFilter(data, 0)
+
+    print(f'data length {len(data)}')
+
+    y = [row[0] for row in data]
+
+    plt.plot(y)
+    plt.xlabel("t")
+    plt.ylabel("IN1")
+    plt.grid()
+    plt.show()
+
+    newData = Utils.processDataResistance(data)
+
+    x = [row[0] for row in newData]
+    y = [row[1] for row in newData]
+
+    plt.plot(x, y)
+    plt.xlabel("Voltage [mV]")
+    plt.ylabel("Current [mA]")
+    plt.grid()
+    plt.show()
+
+def g3_s():
+    with open("data/S_TEST_5.csv", newline="") as file:
+        reader = csv.reader(file, delimiter=';')
+        next(reader)  # pomija nagłówek
+
+        data = [[float(value) for value in row] for row in reader]
+
+    x = [row[0] for row in data]
+    y = [row[1] for row in data]
+
+    plt.plot(x, y)
+    plt.xlabel("IN1")
+    plt.ylabel("IN2")
+    plt.grid()
+    plt.show()
+
+    print(f'data length {len(data)}')
+
+    data = Utils.medianFilter(data, 0)
+
+    print(f'data length {len(data)}')
+
+    x = [row[0] for row in data]
+    y = [row[1] for row in data]
+
+    plt.plot(x, y)
+    plt.xlabel("IN1")
+    plt.ylabel("IN2")
+    plt.grid()
+    plt.show()
+
+    data = Utils.medianFilter(data, 1)
+
+    print(f'data length {len(data)}')
+
+    x = [row[0] for row in data]
+    y = [row[1] for row in data]
+
+    plt.plot(x, y)
+    plt.xlabel("IN1")
+    plt.ylabel("IN2")
+    plt.grid()
+    plt.show()
+
+    newData = Utils.processDataDiode(data)
+
+    x = [row[0] for row in newData]
+    y = [row[1] for row in newData]
+
+    plt.plot(x, y)
+    plt.xlabel("Voltage [mV]")
+    plt.ylabel("Current [uA]")
+    plt.grid()
+    plt.show()
+
+def g3_f():
+    with open("data/F_TEST_3.csv", newline="") as file:
+        reader = csv.reader(file, delimiter=';')
+        next(reader)  # pomija nagłówek
+
+        data = [[float(value) for value in row] for row in reader]
+
+    ## for fast:
+        data = [[value * 1000 for value in row] for row in data]
+    ##end for fast
+
+    x = [row[0] for row in data]
+    y = [row[1] for row in data]
+
+    plt.plot(x, y)
+    plt.xlabel("IN1")
+    plt.ylabel("IN2")
+    plt.grid()
+    plt.show()
+
+    print(f'data length {len(data)}')
+
+    data = Utils.medianFilter(data, 0)
+
+    print(f'data length {len(data)}')
+
+    x = [row[0] for row in data]
+    y = [row[1] for row in data]
+
+    plt.plot(x, y)
+    plt.xlabel("IN1")
+    plt.ylabel("IN2")
+    plt.grid()
+    plt.show()
+
+    data = Utils.medianFilter(data, 1)
+
+    print(f'data length {len(data)}')
+
+    x = [row[0] for row in data]
+    y = [row[1] for row in data]
+
+    plt.plot(x, y)
+    plt.xlabel("IN1")
+    plt.ylabel("IN2")
+    plt.grid()
+    plt.show()
+
+    newData = Utils.processDataDiode(data)
+
+    x = [row[0] for row in newData]
+    y = [row[1] for row in newData]
+
+    plt.plot(x, y)
+    plt.xlabel("Voltage [mV]")
+    plt.ylabel("Current [uA]")
+    plt.grid()
+    plt.show()
+
